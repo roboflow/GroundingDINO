d4345438a7638598d5fb21e59e0c735d252753b2
Qiusheng Wu <giswqs@gmail.com>
2023-05-22 13:27:46 -0400

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
 
     setup(
         name="groundingdino-py",
-        version='0.1.2',
+        version='0.2.0',
         author="International Digital Economy Academy, Shilong Liu",
         url="https://github.com/giswqs/GroundingDINO",
         description="open-set object detector",
